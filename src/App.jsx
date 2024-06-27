c79cf08f0e59521e2f92df33a1bcda1939d3ec1b
mahrryie <mahrryie@gmail.com>
2024-06-27 18:26:32 +0300

    reset app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import Header from "./components/Header";
-import Quiz from "./components/Quiz";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Quiz />
-      </main>
-    </>
-  );
-}
+function App() {}
 
 export default App;
